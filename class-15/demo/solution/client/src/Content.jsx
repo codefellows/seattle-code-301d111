346bd0c40cc206d9c07d5d723f879776afd2d146
John Cokos <highlander.44@gmail.com>
2024-04-12 14:09:49 -0700

    Update Content.jsx

--- class-15/demo/solution/client/src/Content.jsx
+++ class-15/demo/solution/client/src/Content.jsx
@@ -1,30 +1,40 @@
 import { useEffect, useState } from 'react';
+import { withAuth0, useAuth0 } from '@auth0/auth0-react';
 import axios from 'axios';
 
 function Content(props) {
 
+  const {
+    isAuthenticated,
+  } = useAuth0();
+
   const [books, setBooks] = useState([]);
 
-  useEffect(() => {
+  async function getBooks() {
 
-    async function getBooks() {
-      
-      const res = await props.auth0.getIdTokenClaims();
-      const jwt = res.__raw;
+    const res = await props.auth0.getIdTokenClaims();
+    console.log('res', res);
+    const jwt = res.__raw;
 
-      const config = {
-        headers: { "Authorization": `Bearer ${jwt}` },
-        method: 'get',
-        baseURL: import.meta.env.VITE_SERVER_URL,
-        url: '/books'
-      }
-
-      setBooks(booksResponse.data);
+    const config = {
+      headers: { "Authorization": `Bearer ${jwt}` },
+      method: 'get',
+      baseURL: import.meta.env.VITE_SERVER_URL,
+      url: '/books'
     }
 
-    getBooks();
+    const booksResponse = await axios(config);
 
-  }, []);
+    setBooks(booksResponse.data);
+  }
+
+  // this will only run when we are authenticated
+  // and it will run the getBooks() function above to get books
+  useEffect(() => {
+    if( isAuthenticated ) {
+       getBooks();
+    }
+  }, [isAuthenticated]);
 
   return (
     <>
@@ -38,4 +48,4 @@ function Content(props) {
   );
  }
 
- export default Content;
+ export default withAuth0(Content);
